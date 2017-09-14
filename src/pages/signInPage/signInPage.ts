88c4e95079b341f7bf3882a8e9d20e6e69a9ee90
Dikla Tzemah Weyl <Dikla.Weyl@fundtech.isr>
2017-09-14 15:21:05 +0300

    forn sign in validation

--- src/pages/signInPage/signInPage.ts
+++ src/pages/signInPage/signInPage.ts
@@ -1,19 +1,72 @@
 import { Component } from '@angular/core';
-
+import { ViewChild, Directive, ElementRef, OnDestroy, OnInit, Input } from '@angular/core';
+declare var $: any
 
 @Component({
   selector: 'signInPage',
   templateUrl: 'signInPage.html',
   styles: [
-    ``,
+    `
+    body {
+      background-color: #DADADA;
+    }
+    body > .grid {
+      height: 100%;
+    }
+    .image {
+      margin-top: -100px;
+    }
+    .column {
+      max-width: 450px;
+    
+    `,
   ]
 })
-export class SignInPage {
-
+export class SignInPage implements OnInit { 
+@ViewChild('document') documentElementRef: ElementRef;
 
 
   constructor() {
    
   }
 
+  ngOnInit() {
+
+  $(this.documentElementRef.nativeElement)
+    .ready(function() {
+      $('.ui.form')
+        .form({
+          fields: {
+            email: {
+              identifier  : 'email',
+              rules: [
+                {
+                  type   : 'empty',
+                  prompt : 'Please enter your e-mail'
+                },
+                {
+                  type   : 'email',
+                  prompt : 'Please enter a valid e-mail'
+                }
+              ]
+            },
+            password: {
+              identifier  : 'password',
+              rules: [
+                {
+                  type   : 'empty',
+                  prompt : 'Please enter your password'
+                },
+                {
+                  type   : 'length[6]',
+                  prompt : 'Your password must be at least 6 characters'
+                }
+              ]
+            }
+          }
+        })
+      ;
+    })
+  ;
+}
 }
