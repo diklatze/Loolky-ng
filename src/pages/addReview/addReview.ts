520291d11ad07c847f6e5c36f2da5708c825af66
diklatze <diklatzemah@gmail.com>
2017-10-29 16:58:42 +0200

    dd

--- src/pages/addReview/addReview.ts
+++ src/pages/addReview/addReview.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ViewChild, Directive, ElementRef, OnDestroy, OnInit, Input } from '@angular/core';
 import { UtilsServices } from '../../services/utils.services';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 declare var $: any
 
@@ -13,6 +15,19 @@ declare var $: any
   ]
 })
 export class AddReviewPage {
+  productTypes = ['small', 'large'];
+  allProducts = [
+      { name: 'SmallWidget1', type: 'small' },
+      { name: 'SmallWidget2', type: 'small' },
+      { name: 'SmallWidget3', type: 'small' },
+      { name: 'LargeWidget1', type: 'large' },
+      { name: 'LargeWidget2', type: 'large' }
+  ];
+  productsAfterChangeEvent = [];
+  productForm: any;
+
+
+
   cntArr: { value: string, desc: string }[];
   womenCatArr: { value: string, desc: string }[];
   cntFlagArr: { value: string, desc: string, flag: string }[];
@@ -30,7 +45,7 @@ export class AddReviewPage {
   @ViewChild('countryDropdown1') countryDropdownElementRef1: ElementRef;
 
 
-  constructor(utilsServices: UtilsServices) {
+  constructor(utilsServices: UtilsServices,fb: FormBuilder) {
     utilsServices.getListOfCountries().subscribe(
       res => { this.cntFlagArr = res; },
       err => {
@@ -51,7 +66,12 @@ export class AddReviewPage {
           console.error('Register Gov Contract: Error occured on getListOfWomenCatagory');
           console.error(err);
         });
-
+    
+        this.productForm = fb.group({
+          productType: [],
+          product: [],
+          product2: []
+      });
     // this.cntFlagArr.forEach(country => {
     //   country.flag = country.value + ' flag';
     // });
@@ -59,7 +79,18 @@ export class AddReviewPage {
   }
 
 
+  
 
+  
+  // Rebuild the product list every time the product type changes.
+  typeChanged() {
+      const productType = this.productForm.get('productType').value;
+      this.productsAfterChangeEvent = this.allProducts.filter(p => p.type == productType);
+  }
+
+  submitForm() {
+      console.log('Form Data', this.productForm.value);
+  }
 
 
 
